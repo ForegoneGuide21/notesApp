4a35f30013f7503aeb0c8b6cdf1b7b89a827f345
ForegoneGuide21 <166939944+ForegoneGuide21@users.noreply.github.com>
2025-01-18 14:02:33 -0700

    searchbar
    serachbar

--- view/mainmenu.php
+++ view/mainmenu.php
@@ -23,8 +23,20 @@
                     </a>
                 </div>
             </div>
-            <div class="col-8" style="background-color:red">
-                Search bar
+            <div class="col-8">
+                <div class="container" id="bar">
+                    <form action="" method="post">
+                        <div class="row">
+                            <div class="col-10" style="margin-right: -24; !important; background-color: red !important;"  >
+                                <input type="search" class="form-control searchbar" id="search1" placeholder="Search" />
+                            </div>
+                            <div class="col-2" style="background-color: green !important;">
+                                <input type="submit" class="searchbtn" id="searchBTN" value="">
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
             </div>
             <div class="col-2" style="background-color:green">
                 User
@@ -56,6 +68,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
+    <script src="../JS/searchbar.js"></script>
 </body>
 
 </html>
+
