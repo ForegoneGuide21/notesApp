ff1a9f894bc53462a834632dfe74d279fd0e6111
ForegoneGuide21 <166939944+ForegoneGuide21@users.noreply.github.com>
2025-02-22 14:08:33 -0700

    update Co-authored-by: David Hurtado <davidhurtado2000@users.noreply.github.com>

---
+++ controller/ControllerNote.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once '../model/ModelNote.php';
+
+class ControllerNote{
+    public function _ControllerCreateNote($userId): array{
+        try {
+            $obj = new ModelNote();
+            return $obj->_ModelCreateNote($userId);
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+}
--- model/ModelNote.php
+++ model/ModelNote.php
@@ -21,7 +21,7 @@ class ModelNote
             $title = "untitled";
             $description = "untitled";
             $date = date("Y-m-d H:i:s");
-        
+
             $query->bindParam(1, $title);
             $query->bindParam(2, $description);
             $query->bindParam(3, $date);
@@ -33,25 +33,56 @@ class ModelNote
 
         //We need to retrieve the id of the note. We will get the last id to put it into the insert of the note manage
 
+        try {
+            //Prepare is to right the SQL command
+            $obj = connection::singleton();
+            $query = $obj->prepare('SELECT idnotes FROM note ORDER BY idnotes DESC LIMIT 1;');
+
+            $query->execute();
+
+            $vector = $query->fetchAll();
+            $query = null;
+
+
+        } catch (Exception $e) {
+            throw $e;
+        }
+
         //Then we will add it to note Manage
         try {
             $obj = connection::singleton();
-            $query = $obj->prepare('INSERT INTO user (id, name, last_name, username, password) VALUES (NULL, ?, ?, ?, ?)');
+            $query = $obj->prepare('INSERT INTO noteMng (user_iduser, note_idnotes) VALUES (?, ?)');
 
-            $query->bindParam(1, $name);
-            $query->bindParam(2, $last_name);
-            $query->bindParam(3, $username);
-            $query->bindParam(4, $password);
+            $query->bindParam(1, $userId);
+            foreach ($vector as $row) {
+                $query->bindParam(2, $row['idnotes']);
+            }
+
             $query->execute();
 
         } catch (Exception $e) {
             throw $e;
         }
+
+        //RETRIEVE INFORMATION OF USER AND NOTE BY USING NOTE MANAGE
+        try {
+            //Prepare is to right the SQL command
+            $obj = connection::singleton();
+            $query = $obj->prepare('SELECT * FROM noteMng as nm INNER JOIN userT as u ON nm.user_iduser = u.iduser INNER JOIN note as n ON nm.note_idnotes = n.idnotes note_idnotes = ? AND user_iduser = ?');
+
+            $query->bindParam(1, $note_idnotes);
+            $query->bindParam(2, $user_iduser);
+
+            $query->execute();
+
+            $vector = $query->fetchAll();
+            $query = null;
+            return $vector;
+
+        } catch (Exception $e) {
+            throw $e;
+        }
+
+
     }
 }
-
-
-untitled0 -> dog names
-untitled1
-untitled2
-untitled3
